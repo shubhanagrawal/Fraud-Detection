52430389dd4182103417a7b1b056b1cc2741fda2
shubhanagrawal <shubhanagrawal413@gmail.com>
2025-08-29 18:56:55 +0530

    Added Inital Folder Structure and Requirements

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "cnnClassifier"
+project_name = "fraud_detection"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
