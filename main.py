24e67ef31e59d119c468a7b4f4a1f1fefc2ae182
shubhanagrawal <shubhanagrawal413@gmail.com>
2025-08-30 00:05:48 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from src.fraud_detection import logger
 from fraud_detection import logger
+from fraud_detection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
+STAGE_NAME = "Data Ingestion Stage"
 
-logger.info("Starting the main.py script")
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+    except Exception as e:
+           logger.error(f"Error occurred in stage {STAGE_NAME}: {e}")
